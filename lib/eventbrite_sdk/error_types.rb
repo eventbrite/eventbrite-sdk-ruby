58db113044103927af36b1ea5962864245b85ceb
Jeff McKenzie <jeffm@eventbrite.com>
2017-12-14 11:03:05 -0800

    Added some error and status constants for the entire SDK and event. Also added Event#over?

--- lib/eventbrite_sdk/error_types.rb
+++ lib/eventbrite_sdk/error_types.rb
@@ -16,4 +16,13 @@ module EventbriteSDK
 
   # No token was provided for an endpoint that requires authentication
   ERROR_TOKEN_REQUIRED = 'NO_AUTH'.freeze
+
+  # One of the provided arguments is invalid
+  ERROR_ARGUMENTS = 'ARGUMENTS_ERROR'.freeze
+
+  # The package associated to the account does not allow the requested action
+  ERROR_NOT_PERMITTED_BY_PACKAGE = 'INSUFFICIENT_PACKAGE'.freeze
+
+  # No package is associated to the account
+  ERROR_PACKAGE_NOT_SET = 'NO_PACKAGE_SELECTED'.freeze
 end
